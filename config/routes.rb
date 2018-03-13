a9ddc7b0960af2bb62262741611a39985d7265e5
juan diego garcia <juandigt@hotmail.com>
2018-03-13 17:05:59 +0100

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/welcome", to: "pages#welcome"
+  get "/finish_game", to: "pages#finish_game"
+  resources :competences, only: [ :index, :show ]
+  resources :questions, only: [ :show ]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
