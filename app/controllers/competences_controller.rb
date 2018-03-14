0fe3d45a66a26d057038f432ca74364c73842b8b
juan diego garcia <juandigt@hotmail.com>
2018-03-14 11:12:41 +0100

    new changes in show method of competences

--- app/controllers/competences_controller.rb
+++ app/controllers/competences_controller.rb
@@ -6,7 +6,6 @@ class CompetencesController < ApplicationController
 
   def show
     @competence = Competence.find(params[:id])
-
     #creating the call of games of each copetence
     @competence_games = @competence.games.select { |g| g.competence_id == @competence.id }
     random_game
