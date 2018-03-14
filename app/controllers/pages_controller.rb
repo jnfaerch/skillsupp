8d373a201426c0e70f7ff81fb7fc4289b8e93a24
Johan Faerch <jf@keeptrack.nu>
2018-03-14 17:49:16 +0100

    homepage implemented login and design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,22 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  helper_method :resource_name, :resource, :devise_mapping, :resource_class
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mappiing ||= Devise.mappings[:user]
+  end
 
   def home
 
