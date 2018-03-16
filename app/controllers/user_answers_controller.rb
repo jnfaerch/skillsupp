a3cfe7beaa2abc7d4838f99aa00bec969a7b7270
juan diego garcia <juandigt@hotmail.com>
2018-03-16 17:02:57 +0100

    text cklicable and no more error when answer not selected

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -2,18 +2,23 @@ class UserAnswersController < ApplicationController
 
   def create
     @question = Question.find(params[:question_id])
-    @answer = Answer.find(params[:choosen_answer_id])
-    correct = @answer.correct
-    @user_answer = UserAnswer.new(
-      user_id: current_user.id,
-      question_id: params[:question_id],
-      correct: correct
-    )
-    if @user_answer.save
-    redirect_to question_path(@question, {submitted: true})
+    if (params.has_key?(:choosen_answer_id))
+      @answer = Answer.find(params[:choosen_answer_id])
+      correct = @answer.correct
+      @user_answer = UserAnswer.new(
+        user_id: current_user.id,
+        question_id: params[:question_id],
+        correct: correct
+      )
+      if @user_answer.save
+      redirect_to question_path(@question, {submitted: true})
+      else
+        render :new
+      end
     else
-      render :new
+      redirect_to question_path(@question)
     end
+
   end
 
   def update
