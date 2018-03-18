f835e93ac5e00cd9a333b3f3ae2035c1b2d64260
Johan Faerch <jf@keeptrack.nu>
2018-03-18 15:22:54 +0100

    Animation added to bars

--- app/controllers/competences_controller.rb
+++ app/controllers/competences_controller.rb
@@ -3,6 +3,8 @@ class CompetencesController < ApplicationController
   def index
     @competences = Competence.all
     @user = current_user
+    user_correct_answers
+    user_wrong_answers
   end
 
   def show
@@ -14,6 +16,14 @@ class CompetencesController < ApplicationController
 
   def random_game
    @competence.games.sample
+  end
+
+  def user_correct_answers
+    UserAnswer.where(:user_id => @user, :correct => true).count
+  end
+
+  def user_wrong_answers
+    UserAnswer.where(:user_id => @user, :correct => false).count
   end
 
 end
