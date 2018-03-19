5107cc5306015224689e53d90c482836fd6f78f7
Johan Faerch <jf@keeptrack.nu>
2018-03-19 10:01:38 +0100

    JS files isolated

--- app/javascript/competences/question.js
+++ app/javascript/competences/question.js
@@ -1,13 +1,15 @@
 // function for animated progress bars
 $(function animatedquestionprogress() {
-  var current_progress = 0;
-  var interval = setInterval(function() {
-    current_progress = document.getElementById('questionBar').getAttribute('aria-valuenow')
-    $("#questionBar")
-    .css("width", current_progress + "%")
-    .attr("aria-valuenow", current_progress);
-    if (current_progress >= 100)
+  if ($("#question-progress-pages").length > 0){
+    var current_progress = 0;
+    var interval = setInterval(function() {
+      current_progress = document.getElementById('questionBar').getAttribute('aria-valuenow')
+      $("#questionBar")
+      .css("width", current_progress + "%")
+      .attr("aria-valuenow", current_progress);
+      if (current_progress >= 100)
 
-  clearInterval(interval);
-  }, 200);
+    clearInterval(interval);
+    }, 200);
+  }
 });
