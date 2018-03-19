451bd6d32ab7642c430b132fd4e153890998b1ac
juan diego garcia <juandigt@hotmail.com>
2018-03-19 14:56:40 +0100

    creating user_answer correcto/incorrect

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,9 +2,24 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
-    @answers = Answer.all
+    #if we have more of one correct answer, we remove .first and search in all the @correct.answer array
+    @correct_answer = @question.answers.where(correct: true).first
     @game = @question.game
     @next_question = @question.next
+    if find_user_answer
+      @user_answer = find_user_answer
+    else
+      @user_answer = UserAnswer.create(
+        user_id: current_user.id,
+        question_id: params[:id],
+      )
+    end
+  end
+
+  private
+
+  def find_user_answer
+    UserAnswer.find_by(question_id: @question)
   end
 
 end
