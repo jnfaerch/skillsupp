3903ac52f541c28d063a50e7f7f0d20f1ea2b23e
juan diego garcia <juandigt@hotmail.com>
2018-03-20 16:06:14 +0100

    fixing user_answer controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,12 +3,11 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
     @game = @question.game
+    @next_question = @question.next
     if find_user_answer
       @user_answer = find_user_answer
     else
       @user_answer = UserAnswer.new(user_id: current_user.id, question_id: @question.id)
-      # @user_answer.new_record?
-      # not saved in the db yet
     end
   end
 
@@ -16,10 +15,6 @@ class QuestionsController < ApplicationController
   #we use this method for check if we need to create a new user_answer or we update the existing before
   def find_user_answer
     UserAnswer.where(question_id: @question.id, user_id: current_user.id).first
-  end
-
-  def user_answer_params
-    params.require(:user_answer).permit(:question_id)
   end
 end
 
--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -6,36 +6,39 @@ class UserAnswersController < ApplicationController
     # 2. save it in the db
     # 3. if all good, redirect to feedback page
     # (4. else, render the questions#show with the errors)
-    if params[:choosen_answer_id] == @question.answers.where(@answer.correct)
-      params[:correct_answer_id]
+    @question = Question.find(params[:user_answer][:question_id])
+    @user_answer = UserAnswer.new(
+        user_id: current_user.id,
+        question_id: @question.id,
+      )
+    if params[:choosen_answer_id] == @question.answers.where(correct: true)
       @user_answer.correct = true
     else
-      @user_answer.correct = false
-    end
-    params[:user_answer][:chosen_answer_id]
-    params[:user_answer][:question_id]
-    current_user.id
-
-  end
-
-  def update
-    @user_answer = UserAnswer.find(params[:user_answer_id])
-    @question = Question.find(params[:question_id])
-      #we have to check if the choosen_answer is in the array [correct_answer]
-    if params[:choosen_answer_id] == params[:correct_answer_id]
-      # how do we want to update user answer
-      @user_answer.correct = true
-    else
-      # how do we want to update user answer
       @user_answer.correct = false
     end
     @user_answer.save
+    @user_answer.errors.full_messages
+
+    redirect_to question_path(@question, {submitted: true})
+  end
+
+  def update
+    @question = Question.find(params[:user_answer][:question_id])
+    @user_answer = UserAnswer.find(params[:id])
+      #we have to check if the choosen_answer is in the array [correct_answer]
+    if params[:choosen_answer_id] == params[:correct_answer_id]
+      @user_answer.correct = true
+    else
+      @user_answer.correct = false
+    end
+    @user_answer.save
+    @user_answer.errors.full_messages
     redirect_to question_path(@question, {submitted: true})
   end
 
   private
 
   def user_answer_params
-    params.require(:user_answer).permit(:question_id, :choosen_answer_id)
+    params.require(:user_answer).permit(:question_id, :chosen_answer_id, :user_id)
   end
 end
