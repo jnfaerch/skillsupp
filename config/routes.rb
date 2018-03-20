d5d186b627bdc16308520d70e5613e007383529f
Johan Faerch <jf@keeptrack.nu>
2018-03-20 17:55:10 +0100

    routes and files for admin section

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "/welcome", to: "pages#welcome"
   get "/finish_game", to: "pages#finish_game"
+  get "/admin", to: "pages#admin"
   resources :competences, only: [ :index, :show ]
   resources :questions, only: [ :show ]
   resources :user_answers, only: [ :create, :update ]
