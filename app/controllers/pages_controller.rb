4e62fef40f0601130410bbb4aba560a2500b8d88
Johan Faerch <jf@keeptrack.nu>
2018-03-21 15:07:02 +0100

    number of correct questions count added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -28,10 +28,30 @@ class PagesController < ApplicationController
 
   def finish_game
     @competences = Competence.all
+    @user = current_user
+    @questions = Question.all
+    @question_count = number_of_questions_in_game
+    @questions_in_game = questions_in_game
+    @questions_correct = number_of_user_correct
   end
 
   def admin
 
+  end
+
+  private
+
+  def number_of_questions_in_game
+    @q = @user.user_answers.sort_by {|answer| answer.updated_at}.last
+    @q.question.game.questions.size
+  end
+
+  def questions_in_game
+    @q.question.game.questions
+  end
+
+  def number_of_user_correct
+    @user.user_answers.where(question_id: @questions_in_game, correct: true).size
   end
 
 end
