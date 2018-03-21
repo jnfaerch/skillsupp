262d3e5bb5e68fbd1daee1cc28c3ee6a3df5f626
juan diego garcia <juandigt@hotmail.com>
2018-03-21 18:44:28 +0100

    new question with bootstrap collapse

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :competence
   has_many :questions
-
+  default_scope { order(id: :asc) }
   def has_questions
     self.questions[0] ? true : false
   end
