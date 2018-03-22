7c3ff8406b1f2b46fd64bc69766c67ba72734a90
Johan Faerch <jf@keeptrack.nu>
2018-03-22 12:11:42 +0100

    admin started

--- app/javascript/pages/admin.js
+++ app/javascript/pages/admin.js
@@ -2,29 +2,27 @@
 $(function verticalUserProgress() {
   if ($("#ProgressBarAdmin").length > 0){
     var current_progress_admin = 0;
-    var interval = setInterval(function() {
-      current_progress_admin = document.getElementById('goalBarAdmin').getAttribute('aria-valuenow');
-      $("#goalBarAdmin")
-      .css("width", current_progress_admin + "%")
-      .attr("aria-valuenow", current_progress_admin);
-      if (current_progress_admin >= 100)
-
-    clearInterval(interval);
-    }, 500);
+    current_progress_admin = document.getElementById('goalBarAdmin').getAttribute('aria-valuenow');
+    console.log("current_progress_admin is", current_progress_admin);
+    $("#goalBarAdmin").css("width", current_progress_admin + "%");
+    $("#adminTextMobile")
+    .html(current_progress_admin + "%");
+    $("#adminText")
+    .html(current_progress_admin + "%");
   }
 });
 
-$(function verticalTotalProgress() {
-  if ($("#ProgressBarAdmin").length > 0){
-    var current_progress_all = 0;
-    var interval = setInterval(function() {
-      current_progress_all = document.getElementById('goalBarAdminAll').getAttribute('aria-valuenow');
-      $("#goalBarAdminAll")
-      .css("width", current_progress_all + "%")
-      .attr("aria-valuenow", current_progress_all);
-      if (current_progress_all >= 100)
+// $(function verticalTotalProgress() {
+//   if ($("#ProgressBarAdmin").length > 0){
+//     var current_progress_all = 0;
+//     var interval = setInterval(function() {
+//       current_progress_all = document.getElementById('goalBarAdminAll').getAttribute('aria-valuenow');
+//       $("#goalBarAdminAll")
+//       .css("width", current_progress_all + "%")
+//       .attr("aria-valuenow", current_progress_all);
+//       if (current_progress_all >= 100)
 
-    clearInterval(interval);
-    }, 1000);
-  }
-});
+//     clearInterval(interval);
+//     }, 1000);
+//   }
+// });
