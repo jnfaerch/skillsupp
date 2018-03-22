0ffec7b2eb5ec7209ba7dd13563fd1e9c6092230
Johan Faerch <jf@keeptrack.nu>
2018-03-22 12:10:16 +0100

    Navbar updated and competence JS refactored

--- app/javascript/competences/competence.js
+++ app/javascript/competences/competence.js
@@ -3,23 +3,15 @@ $(function animatedTotalProgress() {
   if ($("#competenceProgressBarPage").length > 0){
     var current_progress = 0;
     var current_progress_wrong = 0;
-    var interval = setInterval(function() {
-      current_progress = document.getElementById('goalBar').getAttribute('aria-valuenow');
-      current_progress_wrong = document.getElementById('goalBarWrong').getAttribute('aria-valuenow') * 1;
-      $("#goalBar")
-      .css("width", current_progress + "%")
-      .attr("aria-valuenow", current_progress);
-      $("#goalBarWrong")
-      .css("width", current_progress_wrong + "%")
-      .attr("aria-valuenow", current_progress_wrong);
-      $("#goalText")
-      .html("<br>" + "<i class='far fa-check-circle text-primary'></i>" + " " + current_progress + "%" + " " + "<i class='far fa-times-circle text-danger'></i>" + " " + current_progress_wrong + "%");
-      $("#goalTextMobile")
-      .html("<br>" + "<i class='far fa-check-circle text-primary'></i>" + " " + current_progress + "%" + " " + "<i class='far fa-times-circle text-danger'></i>" + " " + current_progress_wrong + "%");
-      if (current_progress >= 100)
+    current_progress = document.getElementById('goalBar').getAttribute('aria-valuenow');
+    current_progress_wrong = document.getElementById('goalBarWrong').getAttribute('aria-valuenow') * 1;
+    $("#goalBar").css("width", current_progress + "%");
+    $("#goalBarWrong").css("width", current_progress_wrong + "%");
+    $("#goalText")
+    .html("<br>" + "<i class='far fa-check-circle text-primary'></i>" + " " + current_progress + "%" + " " + "<i class='far fa-times-circle text-danger'></i>" + " " + current_progress_wrong + "%");
+    $("#goalTextMobile")
+    .html("<br>" + "<i class='far fa-check-circle text-primary'></i>" + " " + current_progress + "%" + " " + "<i class='far fa-times-circle text-danger'></i>" + " " + current_progress_wrong + "%");
 
-    clearInterval(interval);
-    }, 500);
   }
 });
 
@@ -27,23 +19,17 @@ $(function animatedDailyProgress() {
   if ($("#competenceProgressBarPage").length > 0){
     var daily_progress = 0;
     var daily_progress_wrong = 0;
-    var interval = setInterval(function() {
+    var interval = setTimeout(function() {
       daily_progress = document.getElementById('achievementBar').getAttribute('aria-valuenow');
       daily_progress_wrong = document.getElementById('achievementBarWrong').getAttribute('aria-valuenow');
-      $("#achievementBar")
-      .css("width", daily_progress + "%")
-      .attr("aria-valuenow", daily_progress);
-      $("#achievementBarWrong")
-      .css("width", daily_progress_wrong + "%")
-      .attr("aria-valuenow", daily_progress_wrong);
+      $("#achievementBar").css("width", daily_progress + "%");
+      $("#achievementBarWrong").css("width", daily_progress_wrong + "%");
       $("#achievementText")
       .html("<br>" + "<i class='far fa-check-circle text-primary'></i>" + " " + daily_progress + "%" + " " + "<i class='far fa-times-circle text-danger'></i>" + " " + daily_progress_wrong + "%");
       $("#achievementTextMobile")
       .html("<br>" + "<i class='far fa-check-circle text-primary'></i>" + " " + daily_progress + "%" + " " + "<i class='far fa-times-circle text-danger'></i>" + " " + daily_progress_wrong + "%");
-      if (daily_progress * 1 + daily_progress_wrong * 1 >= 100)
 
-    clearInterval(interval);
-    }, 1000);
+    }, 500);
   }
 });
 
